05990fcd50c7a31abc102144a3fcd4a168078578
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-09-20 00:41:17 -0500

    Made a few changes to the functional component Person and App class component. Added state to the App class component (App.js) as well as a method to handle the switch button event and used the children property of the props object in Person functional component (Person.js).

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-        // JSX
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-          <Person name="Max" age="28" />
-          <Person name="Manu" age="29" >My Hobbies: Racing</Person>
-          <Person name="Stephanie" age="26" />
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stephanie', age: 26 }
+        ]
+    }
+
+    switchNameHandler = () => {
+        console.log('Was clicked!');
+    }
+
+    render() {
+        return (
+            // JSX
+          <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <p>This is really working!</p>
+            <button onClick={this.switchNameHandler}>Switch Name</button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].name} >My Hobbies: Racing</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
+          </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
