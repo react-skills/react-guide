def3b7509153cac66ada7e38459603a7ee90cfcd
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-09-20 02:26:53 -0500

    minor change/correction to the age property in App class component (App.js)

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ class App extends Component {
             <p>This is really working!</p>
             <button onClick={this.switchNameHandler}>Switch Name</button>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].name} >My Hobbies: Racing</Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
           </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
