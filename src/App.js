f8fa40b8644b02ef17a5c6cecbb035ab47456750
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-11-12 06:21:16 -0600

    Added in two way binding of onClick event and input change event with the Person stateless functional component and passed arguments from App stateful component. Also added in some styling for each Person component to look like a card on the page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// Stateful component (aka Smart, Container components)
+// Only want to have a couple (depends on how large the application is);
+// always have less Stateful components than Stateless components
 const app = props => {
     // useState allows us to add State management to functional components (React Hooks)
     // **Don't need to use class based components for State management, but it is the established way though
@@ -20,12 +23,12 @@ const app = props => {
 
     console.log(personsState);
 
-    const switchNameHandler = () => {
+    const switchNameHandler = (newName) => {
         // console.log('Was clicked!');
         // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
         setPersonsState({
             persons: [
-                {name: 'Maximillian', age: 28},
+                {name: newName, age: 28},
                 {name: 'Manu', age: 29},
                 {name: 'Stephanie', age: 27}
             ],
@@ -35,24 +38,57 @@ const app = props => {
         });
     };
 
+    const nameChangedHandler = (event) => {
+        setPersonsState({
+            persons: [
+                {name: 'Max', age: 28},
+                {name: event.target.value, age: 29},
+                {name: 'Stephanie', age: 26}
+            ],
+            // This setPersonsState replaces the personsState (old State) defined above, so we must manually include
+            // all old State data above (this is one way of doing this)
+            // otherState: personsState.otherState
+        });
+    }
+
     // More elegant to use useState
     // useState({otherState: 'some other value'});
     const [otherState, setOtherState] = useState('some other value')
 
     console.log(personsState, otherState);
 
+    // Inline styling
+    const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    };
+
     return (
         // JSX
         <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
-            <button onClick={switchNameHandler}>Switch Name</button>
+            {/* this is one way of passing an argument (probably the more preferred way to do this) */}
+            <button
+                // Style property/style attribute made available by JSX
+                style={style}
+                onClick={switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
             <Person
                 name={personsState.persons[0].name}
                 age={personsState.persons[0].age}
             />
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
-                My Hobbies: Racing
+            <Person
+                name={personsState.persons[1].name}
+                age={personsState.persons[1].age}
+                // define click property which passes a switchHandler as a reference
+                // we can pass methods as props to other components that DO NOT have direct access to the State
+                // this is a common pattern
+                // this is the other way of passing an argument **this can be inefficient
+                click={() => switchNameHandler('Maximilian!!')}
+            changed={nameChangedHandler}>My Hobbies: Racing
             </Person>
             <Person
                 name={personsState.persons[2].name}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Person.css';
 
+// Stateless component and Functional component
+// Good practice to create as many Stateless components (aka Dump, Presentational components) as possible in application
 const person = ( props ) => {
     // JSX
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            {/* onClick can be set to the switchHandler reference from App.js with props.click */}
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            {/* set up two way binding */}
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
